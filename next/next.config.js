c88f57009746924218c9e22ec6cbb92c8e541bc6
matsumatsu233 <cuizhen199209@gmail.com>
2019-08-02 00:32:23 +0900

    Add NO_GOOGLE_ANALYTICS env

--- next/next.config.js
+++ next/next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   publicRuntimeConfig: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    NO_GOOGLE_ANALYTICS: process.env.NO_GOOGLE_ANALYTICS
   }
 };
