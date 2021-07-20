099af976bc57d00910d38e04a67b2db3df0da468
ssdh233 <songsongdahu233@gmail.com>
2021-07-20 22:11:06 +0900

    Set up job on github actions

--- server/src/crawlers/crawler.ts
+++ server/src/crawlers/crawler.ts
@@ -7,6 +7,7 @@ import sleep from "../utils/sleep";
 import { getGeoFromText } from "../utils/googleMapApi";
 import normalizeAddress from "../utils/address";
 import GameCenterModel, { GameCenter, Info } from "../models/gameCenter";
+import { getInstance } from "../utils/address";
 
 export type GameCenterWithRawAddress = Pick<
   GameCenter,
@@ -172,13 +173,13 @@ export default class Crawler {
     let newGameCenterCount = 0;
     for (let i = 0; i < flatResults.length; i++) {
       const gameCenterItem = flatResults[i];
+      console.log(`Processing item ${i}`, gameCenterItem);
 
-      // TODO change here
       let gameCenterEntity = await GameCenterModel.findSameGameCenter(
         gameCenterItem
       );
       if (!gameCenterEntity) {
-        console.log("New Game Center!", gameCenterItem);
+        console.log("New Game Center!");
         newGameCenterCount++;
         gameCenterEntity = new GameCenterModel({
           ...gameCenterItem,
@@ -230,6 +231,7 @@ export default class Crawler {
     );
 
     db.close();
+    getInstance().close();
   }
 }
 
